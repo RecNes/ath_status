5ae31f451bc72f886f798e860ae92c4279386a22
Sencer Hamarat <sencer@dopigo.com>
2021-12-17 17:33:55 +0300

    allowed host changed.

--- ath/settings.py
+++ ath/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env_vars["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env_vars["ALLOWED_HOST"], ]
 
 # Application definition
 
