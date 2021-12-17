e8dd12e4c013c0eb39ac88538b110a783918b04a
Sencer Hamarat <sencer@dopigo.com>
2021-12-17 17:54:13 +0300

    message changed

--- all_time_high/signals.py
+++ all_time_high/signals.py
@@ -6,12 +6,12 @@ from all_time_high.models import AllTimeHighRate
 
 
 def create_message(instance):
-    return f'''Yeni ATH:
+    return f'''⁃ Yeni ATH:
  
-          $ 1.00 = {instance.exchange_rate} ₺
+$ 1.00 = {instance.exchange_rate} ₺ 
 
-ATH: Tüm zamanların en yükseği
-Bu mesaj otomatik olarak oluşturulmuştur.'''
+◦ ATH: Tüm zamanların en yükseği
+◦ Bu mesaj otomatik olarak oluşturulmuştur.'''
 
 
 @receiver(post_save, sender=AllTimeHighRate, dispatch_uid="send_ath_to_twitter")
