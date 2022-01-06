5e12bd282597b007058e75ecb1bd858b0a724f8b
Sencer Hamarat <sencer@dopigo.com>
2022-01-06 22:42:44 +0300

    Alter management command and get_exchange_rate method according to new models

--- all_time_high/management/commands/fetchexchangerates.py
+++ all_time_high/management/commands/fetchexchangerates.py
@@ -1,31 +1,17 @@
-import json
-from decimal import Decimal
-
 from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext_lazy as _
-from google_currency import convert
 
-from all_time_high.models import AllTimeHighRate
+from all_time_high.views import get_exchange_rate
 
 
 class Command(BaseCommand):
     help = _("Kur fiyatlarını çeker")
 
     def handle(self, *args, **options):
-        response = json.loads(convert("usd", "try", 1))
-        currency, created = AllTimeHighRate.objects.get_or_create(
-            currency_1=response["from"].lower(),
-            currency_2=response["to"].lower(),
-        )
-
-        currency.exchange_rate = Decimal(response["amount"]).quantize(Decimal("0.00"))
-        if currency.all_time_high_rate is None or currency.exchange_rate > currency.all_time_high_rate:
-            currency.all_time_high_rate = currency.exchange_rate
-            currency.notify = True
-        currency.save()
+        currency = get_exchange_rate(from_currency="usd", to_currency="try", currency_amount=1)
 
         self.stdout.write(
             self.style.SUCCESS(
-                f'"{currency.currency_1}" x "{currency.currency_2}" : {currency.exchange_rate}'
+                f'"{currency.base.upper()}" x "{currency.target.upper()}" {currency.exchangerate_set.last()}'
             )
         )
--- all_time_high/views.py
+++ all_time_high/views.py
@@ -1,8 +1,99 @@
-from django.shortcuts import render
-from google_currency import convert
+from decimal import Decimal
 
-# Create your views here.
-from all_time_high.models import AllTimeHighRate
+from django.shortcuts import render
+
+from all_time_high.api import from_google_exchange_rates, from_open_exchange_rates, from_abstractapi_exchange_rates
+from all_time_high.models import ExchangeCurrency, AllTimeHigh, OneUnitDropped, ExchangeRate
+
+
+def get_higher_rate(latest_rates) -> float:
+    """
+    return highest rate
+    :param latest_rates:
+    :return:
+    """
+    values = [i for i in latest_rates.values() if i is not None]
+    rate = 0
+    if len(values):
+        values.sort(reverse=True)
+        rate = values[0]
+    return rate
+
+
+def get_lowest_rate(latest_rates) -> float:
+    """
+    return lowest rate
+    :param latest_rates:
+    :return:
+    """
+    values = [i for i in latest_rates.values() if i is not None]
+    rate = 0
+    if len(values):
+        values.sort()
+        rate = values[0]
+    return rate
+
+
+def get_exchange_rate(from_currency="usd", to_currency="try", currency_amount=1) -> object:
+    """
+    Fetch exchnge rate and save to DB and return object.
+    :param from_currency:
+    :param to_currency:
+    :param currency_amount:
+    :return:
+    """
+    kwargs = {
+        "from_currency": from_currency,
+        "to_currency": to_currency,
+        "currency_amount": currency_amount
+    }
+
+    latest_rates = {
+        "google_rate": from_google_exchange_rates(**kwargs),
+        "oxr_rate": from_open_exchange_rates(**kwargs),
+        "abstractapi_rate": from_abstractapi_exchange_rates(**kwargs),
+    }
+
+    highest_rate = get_higher_rate(latest_rates)
+    lowest_rate = get_lowest_rate(latest_rates)
+
+    currency, created = ExchangeCurrency.objects.get_or_create(
+        base=from_currency.lower(),
+        target=to_currency.lower(),
+    )
+    current_exchange_rate = Decimal(highest_rate).quantize(Decimal("0.00"))
+    exchange_rate_obj = ExchangeRate(
+        exchange_rate=current_exchange_rate
+    )
+    currency.exchangerate_set.add(exchange_rate_obj, bulk=False)
+
+    try:
+        all_time_high = currency.alltimehigh
+    except AllTimeHigh.DoesNotExist:
+        all_time_high = None
+
+    if all_time_high is None or current_exchange_rate > all_time_high.exchange_rate:
+        all_time_high = AllTimeHigh.objects.get_or_create(
+            currency=currency
+        )
+        all_time_high.exchange_rate = current_exchange_rate
+        all_time_high.notify = True
+        all_time_high.save()
+
+    try:
+        one_unit_drop = currency.oneunitdropped
+    except OneUnitDropped.DoesNotExist:
+        one_unit_drop = None
+
+    if one_unit_drop is None or one_unit_drop.exchange_rate - lowest_rate >= 1:
+        one_unit_drop = OneUnitDropped.objects.get_or_create(
+            currency=currency
+        )
+        one_unit_drop.exchange_rate = lowest_rate
+        one_unit_drop.notify = True
+        one_unit_drop.save()
+
+    return currency
 
 
 def one_page_view(request):
