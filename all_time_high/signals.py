84b821b12193eb2e940e5dc45db9af2fc3160758
Sencer Hamarat <sencer@dopigo.com>
2022-02-06 00:24:14 +0300

    Fix for notification via signals

--- all_time_high/signals.py
+++ all_time_high/signals.py
@@ -1,17 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from all_time_high.management.commands.notify import get_notification_settings
+from all_time_high.management.commands import notify
 from all_time_high.models import AllTimeHigh, OneUnitDropped
 
 
 @receiver(post_save, sender=AllTimeHigh, dispatch_uid="post_ath_message")
 def post_ath_message(sender, instance, raw, *args, **kwargs):
-    notification_settings = get_notification_settings()
-    post_ath_message(notification_settings)
+    notification_settings = notify.get_notification_settings()
+    notify.post_ath_message(notification_settings)
 
 
 @receiver(post_save, sender=OneUnitDropped, dispatch_uid="post_oud_message")
 def post_oud_message(sender, instance, raw, *args, **kwargs):
-    notification_settings = get_notification_settings()
-    post_oud_message(notification_settings)
+    notification_settings = notify.get_notification_settings()
+    notify.post_oud_message(notification_settings)
