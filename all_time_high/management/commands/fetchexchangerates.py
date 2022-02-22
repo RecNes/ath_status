6325d1aa1e89ee12dda442638bc79f60f5710cb0
Sencer Hamarat <sencer@dopigo.com>
2022-02-23 01:00:42 +0300

    Change fetch exchange rates command behaviour to take currencies from db.

--- all_time_high/management/commands/fetchexchangerates.py
+++ all_time_high/management/commands/fetchexchangerates.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext_lazy as _
 
+from all_time_high.models import ExchangeCurrency, ExchangeRate
 from all_time_high.views import get_exchange_rate
 
 
@@ -8,10 +9,19 @@ class Command(BaseCommand):
     help = _("Kur fiyatlarını çeker")
 
     def handle(self, *args, **options):
-        currency = get_exchange_rate(from_currency="usd", to_currency="try", currency_amount=1)
+        exchange_currencies = ExchangeCurrency.objects.all()
+        for exchange_currency in exchange_currencies:
+            try:
+                currency = get_exchange_rate(
+                    from_currency=exchange_currency.base,
+                    to_currency=exchange_currency.target,
+                    currency_amount=1)
 
-        self.stdout.write(
-            self.style.SUCCESS(
-                f'"{currency.base.upper()}" x "{currency.target.upper()}" {currency.exchangerate_set.last()}'
-            )
-        )
+                last_rate = ExchangeRate.objects.filter(currency=currency).last()
+                self.stdout.write(
+                    self.style.SUCCESS(f"{last_rate}")
+                )
+            except Exception as uee:
+                self.stdout.write(
+                    self.style.ERROR(f"{exchange_currency}:\r\n{uee}")
+                )
