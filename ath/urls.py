86f511fa43d75dc6dfadb478a71677f26038ff27
Sencer HAMARAT <sencerhamarat@gmail.com>
2025-10-01 17:15:35 +0300

    Performance tuning
    Signed-off-by: Sencer HAMARAT <sencerhamarat@gmail.com>

--- ath/urls.py
+++ ath/urls.py
@@ -17,10 +17,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
-from all_time_high.views import one_page_view, ExchangeGraphView
+from all_time_high.views import one_page_view, ExchangeGraphView, BulkExchangeGraphView
 
 urlpatterns = [
     path(f"{settings.DJANGO_ADMIN_URI}/", admin.site.urls),
     path("", one_page_view, name="main"),
-    path("get-chart-data/<int:pk>", ExchangeGraphView.as_view(), name="get-chart-data")
+    path("get-chart-data/<int:pk>", ExchangeGraphView.as_view(), name="get-chart-data"),
+    path("get-all-chart-data/", BulkExchangeGraphView.as_view(), name="get-all-chart-data"),
 ]
